323983ac94d9077d9bb40bd6b9d96ca1cb7b7115
saikhna08 <saikhna08@gmail.com>
2021-07-08 00:56:52 -0600

    fix background video for mobile

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,16 +6,25 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video class="background_video" playsinline loop muted autoPlay preload="auto" poster="images/background_pic.png">
-        <source src="/videos/Yosuke_v2_compressed.mp4" type="video/mp4" />
-      </video>
+      <video
+        class="background_video"
+        src="/videos/Yosuke_v2_compressed.mp4"
+        playsinline
+        loop
+        muted
+        autoPlay
+        poster="images/background_pic.png"
+      ></video>
 
       <h1>Sakana Sushi Bar & Japanese Cuisine</h1>
       <p>DINE-IN & PICK UP AVAILABLE!</p>
-      <a class="button" href="https://www.customer2you.com/SakanaSushiBar.nsf/" target="_blank">
+      <a
+        class="button"
+        href="https://www.customer2you.com/SakanaSushiBar.nsf/"
+        target="_blank"
+      >
         <span>Order Online</span>
       </a>
-
     </div>
   );
 }
